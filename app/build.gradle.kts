90a85e6dab8d1d9b6d12b315db6fe34689d75c03
sls1005 <sls1005@github.com>
2024-11-18 22:01:14 +0800

    Fix typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "test.com.github.www.sls1005.htmlrealizer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
