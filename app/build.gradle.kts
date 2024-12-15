345e38e98c66d3fc0b689f646533c6b5cec3fedf
sls1005 <90055573+sls1005@users.noreply.github.com>
2024-12-15 22:16:42 +0800

    Make scrollable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "test.com.github.www.sls1005.htmlrealizer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
